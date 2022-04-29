c002ba03a5d8ca02cef7a0ee64fc5fee77f5bdf6
Colm Britton <colmjude@gmail.com>
2022-04-29 14:51:04 +0100

    Adds basic shared tooling

---
+++ package/package-scripts.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+let scripts = {}
+
+function readJsonFile (filePath) {
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'))
+}
+
+let configPaths = require('./default.paths.json')
+const optionalConfigPath = 'wra-frontend.config.json'
+
+if (fs.existsSync(optionalConfigPath)) {
+  configPaths = {
+    ...configPaths,
+    ...readJsonFile(optionalConfigPath)
+  }
+}
+
+scripts.build = {
+  stylesheets: `npx node-sass ${configPaths.scssPath} -o ${configPaths.stylesheetsOutputPath} --include-path ${configPaths.WRAFrontendPath}`
+}
+
+scripts.watch = {
+  stylesheets: `npx chokidar ${configPaths.watchPaths} -c "npm run nps build.stylesheets"`
+}
+
+module.exports = { scripts }
