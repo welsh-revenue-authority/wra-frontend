4aeccfab58894a1678151cfa4b70658e71b076f0
Colm Britton <colmjude@gmail.com>
2022-05-06 15:17:10 +0100

    Creates a basic python package
    Includes a minimal base template

---
+++ setup.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+from setuptools import setup, find_packages
+
+components = []
+directories_jinja = glob.glob("wra_frontend/**/**/*.jinja", recursive=True)
+
+for directory in directories_jinja:
+    components.append(
+        os.path.relpath(os.path.dirname(directory), "wra_frontend")
+        + "/*.jinja"
+    )
+
+setup(
+    name="wra-frontend",
+    version="0.1.0",
+    author="WRA team",
+    description="Reusable frontend code for wra services and products",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
+    package_data={"wra-frontend": components},
+    python_requires=">=3.5",
+    install_requires=["validators"],
+    include_package_data=True,
+    extras_require={"testing": ["pytest", "flake8"]},
+)
